8e421ca7f363ed88fea0c62d5ab0c613548442eb
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-11 21:25:11 +0300

    adding icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
+import { BsFillPlusSquareFill } from "react-icons/bs";
+import { TbTrashOff } from "react-icons/tb";
 
 const App = () => {
   return (
     <div className="container">
+      <h1>Todo App</h1>
       <form className="form">
         <div className="form-row">
           <input type="text" id="item" placeholder="Add a new todo..." />
         </div>
-        <button className="btn">Add</button>
+        <BsFillPlusSquareFill className="btn" />
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
@@ -17,12 +20,12 @@ const App = () => {
             <input type="checkbox" />
             Item 1
           </label>
-          <button className="btn">Delete</button>
+          <TbTrashOff className="btn" />
           <label htmlFor="">
             <input type="checkbox" />
             Item 2
           </label>
-          <button className="btn">Delete</button>
+          <TbTrashOff className="btn" />
         </li>
       </ul>
     </div>
