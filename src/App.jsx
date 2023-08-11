407407de6423e64d0a02ba267faa854632c1bcd2
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-11 20:39:28 +0300

    finishing jsx part

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <form className="form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      <form className="container">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn">Delete</button>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 };
 
