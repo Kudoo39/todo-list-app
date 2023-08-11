d4a6b4a5a38bb8801cde273419669e3625d7d64c
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-11 23:44:30 +0300

    managing state and adding todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BsFillPlusSquareFill } from "react-icons/bs";
 import { TbTrashOff } from "react-icons/tb";
 
 const App = () => {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [...currentTodos, { title: newItem, completed: false }];
+    });
+  };
+
+  console.log(todos);
+
   return (
     <div className="container">
       <h2>Todo App</h2>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <div className="form-row">
-          <input type="text" id="item" placeholder="Add a new todo..." />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+            placeholder="Add a new todo..."
+          />
         </div>
-        <BsFillPlusSquareFill className="btn add" />
+        <BsFillPlusSquareFill onClick={handleSubmit} className="btn add" />
       </form>
       <h2>Todo List</h2>
       <ul className="list">
-        <li>
-          <label htmlFor="">
-            <input className="input" type="checkbox" />
-            Item 1
-            <TbTrashOff className="btn remove" />
-          </label>
-
-          <label htmlFor="">
-            <input className="input" type="checkbox" />
-            Item 2
-            <TbTrashOff className="btn remove" />
-          </label>
-        </li>
+        {todos.map((todo) => {
+          <li>
+            <label htmlFor="">
+              <input checked={todo.completed} type="checkbox" />
+              {todo.title}
+              <TbTrashOff className="btn remove" />
+            </label>
+          </li>;
+        })}
       </ul>
     </div>
   );
